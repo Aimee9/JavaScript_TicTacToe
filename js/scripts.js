2b59a3954fe077e21acb046780cceb02df9d6e85
aimee reiss & mlmendeznunez <student@epicodus.com>
2015-09-23 11:25:07 -0700

    Specs for functions space, player, and board

--- js/scripts.js
+++ js/scripts.js
@@ -3,38 +3,30 @@ var space = [];
 var board = [];
 
 function Player( pname, symbol, score) {
-    this.pname = name;
+    this.pname = pname;
     this.symbol = symbol;
     this.score = score;
     player.push(this);
 }
 
-function Space( coor1, coor2 ) { //Make prototype constructors capital S
-    this.coor1 = coor1;
-    this.coor2 = coor2;
+function Space( coorx, coory ) { //Make prototype constructors capital S
+    this.coorX = coorx;
+    this.coorY = coory;
     space.push(this);
     //space.markby(player);
 }
 
-var bottomleft = new Space(["1", "1"]);
-// var bottomcenter = new Space("1", "2");
-// var bottomright = new Space("1", "3");
-// var middleleft = new Space("2", "1");
-// var middlecenter = new Space("2", "2");
-// var middleright = new Space("2", "3");
-// var topleft = new Space("3", "1");
-// var topcenter = new Space("3", "2");
-// var topright = new Space("3", "3");
-
-// var Board ( space[]){
-  // for(var i=0; i < space.length; i++) {
-  //    board.push(space[i]);
-  // } return board [];
-// }
-
-// function board( space[bottomleft, bottomcenter, bottomright, middleleft, middlecenter, middleright, topleft, topcenter, topright ] ){
-//
-// }
+function Board( ){
+  this.bottomleft = new Space("1", "1");
+  this.bottomcenter = new Space("1", "2");
+  this.bottomright = new Space("1", "3");
+  this.middleleft = new Space("2", "1");
+  this.middlecenter = new Space("2", "2");
+  this.middleright = new Space("2", "3");
+  this.topleft = new Space("3", "1");
+  this.topcenter = new Space("3", "2");
+  this.topright = new Space("3", "3");
+}
 
 
 //
--- spec/specs.js
+++ spec/specs.js
@@ -1,12 +1,23 @@
-describe('space', function() {
-  it('will create a coordinate for the board', function() {
-    var newSpace = new Space("1", "3");
-    expect(newSpace.coor1).to.equal("1");
+describe('player', function() {
+  it('will create a player for the game', function() {
+    var newPlayer = new Player("bob", "x", 3);
+    expect(newPlayer.pname).to.equal("bob");
+    expect(newPlayer.symbol).to.equal("x");
+    expect(newPlayer.score).to.equal(3);
   });
 });
 
+describe('space', function() {
+  it('will create a coordinate for the board', function() {
+    var newSpace = new Space("1", "3");
+    expect(newSpace.coorX).to.equal("1");
+    expect(newSpace.coorY).to.equal("3");
+  });
+});
 
-// it('will create a coordinate for the board', function() {
-//   var testCoordinates = new space("1", "3");
-//   expect(space([0])).to.eql(testCoordinates);
-// });
+describe('board', function() {
+  it('will create a tic tac toe board', function() {
+    var newBoard = new Board();
+    expect(newBoard.bottomleft.coorX).to.eql("1");
+  });
+});
